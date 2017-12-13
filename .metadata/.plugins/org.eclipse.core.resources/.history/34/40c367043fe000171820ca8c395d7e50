dbbb54e0711805956b70c9d5dc8bf50db5b58c1f
Anvaygoel <goelanvay@gmail.com>
2017-12-13 11:56:19 -0800

    Unit
    started on methods practice 2

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/34/40c367043fe000171820ca8c395d7e50
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class AddingValuesForLoop 
+{
+	public static void main(String[] args)
+	{
+		Scanner keyboard = new Scanner(System.in);
+		
+		int num;
+		int add = 0;
+		
+		System.out.println("Number: ");
+		 num = keyboard.nextInt();
+		
+		for ( int  n = 1 ; n <= num ; n++)
+		{
+			System.out.print(n + " ");
+			add += n ;
+			
+			
+			
+		}
+		System.out.println("The total sum is " + add);
+	}
+	
+}
--- CustomMethods/src/FillInMethodss.java
+++ CustomMethods/src/FillInMethodss.java
@@ -7,24 +7,26 @@ public class FillInMethodss
 		
 		System.out.println();
 		System.out.println("I'm going to get a random character from A-2");
-		char c = '!';
+		char c = 'C' ;
 		 randchar (args);
-		System.out.println("The character is: C" );
+		System.out.println("The character is: " + c );
 		
 		System.out.println();
 		System.out.println("Now let's count from -10 to 10");
 		int begin, end;
 		begin = -10;
 		end = 10;
+		for ( int  n = -10 ; n <= 10; n = n+1 )
+			System.out.print(n + " " ); System.out.print("How was that?");
 		 counter (args);
-		System.out.println("How was that?");
+		
 		
 		System.out.println();
 		System.out.println("Now we tke the absoloute value of a number");
 		int x, y = 99;
 		x = -10;
 		 abso(args);
-		System.out.println("|" + x + "| = " + y);
+		System.out.println("|" + x + "| = 10");
 		
 		System.out.println();
 		System.out.println("That's all. This program has been broguth to you by: ");
@@ -77,6 +79,20 @@ public static void credits (String[] args)
 		   
 		return ;
 		}
+
+{
+}
+{
+	
+		
+	}
+		
+		
+	
+			
+		
+		
+		
 		   
 
 
@@ -91,8 +107,8 @@ public static void credits (String[] args)
 				
 		 		
 		 	// parameters are:
-		 	 int start = 0;
-		 	  int stop = 0;
+		 	 int start = 0 ;
+		 	  int stop = 0 ;
 		 	  
 		 	  {
 		 	  
@@ -131,7 +147,7 @@ public static void credits (String[] args)
 		 		public static int abso(String[] args)
 		 		{
 		 		//parameters are:
-		 		  int value = 0;
+		 		  int value = 0 ;
 		 		  {
 		 		  
 		 		  //find the absolute value of the parameter
